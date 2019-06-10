debc7213996e06aa5496887ebc67fc0b70a949e3
Peter Aichinger <p.aichinger@googlemail.com>
2019-06-10 21:53:13 +0200

    Fix columns

--- pages/index.vue
+++ pages/index.vue
@@ -1,42 +1,52 @@
 <template>
   <v-content fluid>
     <v-container class="pa-0 ma-0">
-      <v-layout row wrap>
-        <v-card>
-          <v-card-title>
-            Welcome to TOPhub
-          </v-card-title>
-          <v-card-text>
-            <v-flex xs12 sm8 md3>
-              <v-card>
-                <v-card-title>
-                  General
-                </v-card-title>
-              </v-card>
-              <v-card>
-                <v-card-title>
-                  Development
-                </v-card-title>
-              </v-card>
-              <v-card>
-                <v-card-title>
-                  Support
-                </v-card-title>
-              </v-card>
-              <v-card>
-                <v-card-title>
-                  Product Managemen
-                </v-card-title>
-              </v-card>
-              <v-card>
-                <v-card-title>
-                  ICT
-                </v-card-title>
-              </v-card>
-            </v-flex>
-          </v-card-text>
-        </v-card>
-      </v-layout>
+      <v-card fluid>
+        <v-card-title fluid>
+          Welcome to TOPhub
+        </v-card-title>
+        <v-card-text fluid>
+          <v-container grid-list-md text-xs-center>
+            <v-layout row wrap>
+              <v-flex xs4>
+                <v-card fluid>
+                  <v-card-title fluid>
+                    General
+                  </v-card-title>
+                </v-card>
+              </v-flex>
+              <v-flex xs4>
+                <v-card>
+                  <v-card-title>
+                    Development
+                  </v-card-title>
+                </v-card>
+              </v-flex>
+              <v-flex xs4>
+                <v-card>
+                  <v-card-title>
+                    Support
+                  </v-card-title>
+                </v-card>
+              </v-flex>
+              <v-flex xs4>
+                <v-card>
+                  <v-card-title>
+                    Product Management
+                  </v-card-title>
+                </v-card>
+              </v-flex>
+              <v-flex xs4>
+                <v-card>
+                  <v-card-title>
+                    ICT
+                  </v-card-title>
+                </v-card>
+              </v-flex>
+            </v-layout>
+          </v-container>
+        </v-card-text>
+      </v-card>
     </v-container>
   </v-content>
 </template>
